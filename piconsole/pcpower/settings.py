900af368840ed5fb52ff2a4d3a0d15e75350b9a5
charredchar <charredchar@gmail.com>
2020-01-03 22:33:26 -0600

    Update settings.py

--- piconsole/pcpower/settings.py
+++ piconsole/pcpower/settings.py
@@ -4,11 +4,10 @@ PCS = {
     'Blackbox': {
         'display_name': 'Blackbox',
         'ip': '192.168.1.111,192.168.5.112',
-        #Mains changed to Reset switch, leave this as False.
         'mains_normally_closed': False,
         'gpio': {
             'power': 17,
-            'reset': 18,
+            'mains_power': 18,
             'power_led': 26
         }
     },
@@ -18,7 +17,7 @@ PCS = {
         'mains_normally_closed': False,
         'gpio': {
             'power': 22,
-            'reset': 23,
+            'mains_power': 23,
             'power_led': 19
         }
     }
