ad7d8e81a323f05a82a36ad00bebc4641ecd5b9d
charredchar <charredchar@gmail.com>
2020-01-03 22:31:24 -0600

    Update settings.py

--- piconsole/pcpower/settings.py
+++ piconsole/pcpower/settings.py
@@ -1,12 +1,25 @@
+#Pins use BCM numbers.
+
 PCS = {
-    'main': {
-        'display_name': 'My PC',
-        'ip': '192.168.1.5',
-        'mains_normally_closed': True,
+    'Blackbox': {
+        'display_name': 'Blackbox',
+        'ip': '192.168.1.111,192.168.5.112',
+        #Mains changed to Reset switch, leave this as False.
+        'mains_normally_closed': False,
         'gpio': {
-            'power': 20,
-            'mains_power': 26,
-            'power_led': 21
+            'power': 17,
+            'reset': 18,
+            'power_led': 26
+        }
+    },
+    'KH-PrimeRadiant': {
+        'display_name': 'KH-PrimeRadiant',
+        'ip': '192.168.1.113',
+        'mains_normally_closed': False,
+        'gpio': {
+            'power': 22,
+            'reset': 23,
+            'power_led': 19
         }
     }
 }
